chameleon-protocol/chameleon

commit 638fbad0a3d28d7c28d4529ed4288f1998cb0c30

fix: use homedir as workdir in systemd unit
as default acme storage is $workdir/acme instead of $homedir/.config/...

scripts/install_server.sh
@@ -646,7 +646,7 @@ After=network.target
 [Service]
 Type=simple
 ExecStart=$EXECUTABLE_INSTALL_PATH server --config ${_config_name}.yaml
-WorkingDirectory=$CONFIG_DIR
+WorkingDirectory=$HYSTERIA_HOME_DIR
 User=$HYSTERIA_USER
 Group=$HYSTERIA_USER
 Environment=HYSTERIA_LOG_LEVEL=info

commit b4852059b06f51b8f22202b38ac71e1004d387ac

fix: confdir in systemd unit in install_server.sh
caused by 638fbad0a3d28d7c28d4529ed4288f1998cb0c30

scripts/install_server.sh
@@ -649,7 +649,7 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=$EXECUTABLE_INSTALL_PATH server --config ${_config_name}.yaml
+ExecStart=$EXECUTABLE_INSTALL_PATH server --config ${CONFIG_DIR}/${_config_name}.yaml
 WorkingDirectory=$HYSTERIA_HOME_DIR
 User=$HYSTERIA_USER
 Group=$HYSTERIA_USER