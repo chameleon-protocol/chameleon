12ac1f508cd1218a27149553e1c23e0f1be0605c
chameleon <chameleon@users.noreply.github.com>
2026-08-13 15:21:01 +0800

    license: drop the per-file notices
    The license lives in LICENSE and LICENSE.MIT. Repeating it at the top of every
    file added 14 lines of noise to each one and bought nothing a fork needs.

--- app/internal/linkmon/watch_bsd.go
+++ app/internal/linkmon/watch_bsd.go
@@ -1,19 +1,5 @@
 //go:build darwin || freebsd || netbsd || openbsd || dragonfly
 
-// chameleon -- a censorship-resistant transport
-// Copyright (C) 2026 The chameleon authors
-//
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License version 3 as published by the Free
-// Software Foundation.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-// PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program. If not, see <https://www.gnu.org/licenses/>.
-
 package linkmon
 
 import (
